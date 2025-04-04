0757ed2bd744689fbdc704502f086397a2732995
kevinDomingues <kevincarvas@gmail.com>
2025-04-04 18:24:29 +0100

    feat(K-1): initial project config

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import DummyComponent from './components/DummyComponent'
+
+function App() {
+  return (
+    <>
+      <DummyComponent />
+    </>
+  )
+}
+
+export default App
